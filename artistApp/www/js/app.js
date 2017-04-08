222106158a6bed5af66e611dcca6c6f0d6614228
Nathan Pickard <nathan.pickard87@gmail.com>
2017-04-07 21:11:02 -0700

    Sub-page added to list view and added global header

---
+++ artistApp/www/js/app.js
@@ -0,0 +1,94 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        // cordova.plugins.Keyboard.disableScroll(true);
+
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('tabs', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      .state('tabs.home', {
+        url: '/home',
+        views: {
+          'home-tab': {
+            templateUrl: 'templates/home.html'
+          }
+        }
+      })
+
+      .state('tabs.list', {
+        url: '/list',
+        views: {
+          'list-tab': {
+            templateUrl: 'templates/list.html',
+            controller: 'ListController'
+          }
+        }
+      })
+
+      .state('tabs.detail', {
+        url: '/list/:aId',
+        views: {
+          'list-tab': {
+            templateUrl: 'templates/detail.html',
+            controller: 'ListController'
+          }
+        }
+      })
+
+    $urlRouterProvider.otherwise('/tab/home');
+  })
+
+  .controller('ListController', ['$scope', '$http', '$state',
+    function ($scope, $http, $state) {
+      $http.get('js/data.json').success(function (data) {
+        $scope.artists = data;
+        $scope.whichartist = $state.params.aId;
+        $scope.data = { showDelete: false, showReorder: false };
+
+        $scope.onItemDelete = function (item) {
+          $scope.artists.splice($scope.artists.indexOf(item), 1);
+        }
+
+        $scope.doRefresh = function () {
+          $http.get('js/data.json').success(function (data) {
+            $scope.artists = data;
+            $scope.$broadcast('scroll.refreshComplete');
+          });
+        }
+
+        $scope.toggleStar = function (item) {
+          item.star = !item.star;
+        }
+
+
+        $scope.moveItem = function (item, fromIndex, toIndex) {
+          $scope.artists.splice(fromIndex, 1);
+          $scope.artists.splice(toIndex, 0, item);
+        };
+      });
+    }]);
